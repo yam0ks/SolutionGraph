d196f83e33e3681e2f05ccdd03fa3d791c588f09
halisha <vadimkaz.alx@gmail.com>
2022-03-27 19:22:28 +0300

    Реорганизовано поведение модели

--- app/src/main/java/com/model/Model.java
+++ app/src/main/java/com/model/Model.java
@@ -2,76 +2,26 @@ package com.model;
 
 public class Model {
     private Simplex simplex;
-    private Simplex.InputData inputData;
-    private Simplex.OutputData outputData;
-    private int restrictionCount;
-    private int variableCount;
-
-    private enum MinMax{
-        MIN,
-        MAX
-    }
+    private Simplex.OutputData simplexOutputData;
+    //private Graph.OutputData graphOutputData;
+    //private Graph graph;
 
     public Model(){
         simplex = new Simplex();
         //graph = new Graph();
     }
 
-    private void initializeData(){
-        inputData = new Simplex.InputData();
-        outputData = new Simplex.OutputData();
+    private Simplex.OutputData getSimplexSolution(Restriction[] Restrictions, MainFunc mainFunc){
+        simplexOutputData = new Simplex.OutputData();
+        simplexOutputData = simplex.getResult(Restrictions, mainFunc);
+        return simplexOutputData;
     }
 
-    private void setCounts(int restrictionCount, int variableCount){
-        this.restrictionCount = restrictionCount;
-        this.variableCount = variableCount;
-    }
-
-    private Simplex.OutputData getSimplexSolution(double[][] rawRestrictions, double[] rawMainFunc){
-        initializeData();
-        rawDataToFraction(rawRestrictions, rawMainFunc);
-        simplex.SetInputData(inputData);
-        outputData = simplex.startWork();
-        return outputData;
-    }
-
-    private Simplex.OutputData getGraphSolution(double[][] rawRestrictions, double[] rawMainFunc){
+    private Graph.OutputData getGraphSolution(Restriction[] Restrictions, MainFunc mainFunc){
         //Надо сначала сделать graph
-        return outputData;
+        //graphOutputData = new Graph.OutputData();
+        //graphOutputData = graph.getResult(Restrictions, mainFunc);
+        //return graphOutputData;
     }
 
-    private void rawDataToFraction(double[][] rawRestrictions, double[] rawMainFunc){
-        Fraction[][] restrictions = new Fraction[variableCount + 2][restrictionCount];
-        Fraction[] mainFunc = new Fraction[variableCount + 1];
-        Simplex.Sign[] signs = new Simplex.Sign[restrictionCount];
-        boolean findMax;
-
-        for (int i = 0; i < variableCount + 2; i++){
-            for(int j = 1; j < restrictionCount; j++){
-                restrictions[i][j - 1] = new Fraction(rawRestrictions[i][j]);
-            }
-        }
-
-        for (int i = 1; i < variableCount + 1; i++)
-        {
-            mainFunc[i - 1] = new Fraction(rawMainFunc[i]);
-        }
-
-        for (int i = 0; i < restrictionCount; i++)
-        {
-            signs[i] = Simplex.Sign.values()[(int)rawMainFunc[i]];
-        }
-
-        findMax = rawMainFunc[0] == MinMax.MAX.ordinal();
-
-        setInputDataParams(restrictions, mainFunc, signs, findMax);
-    }
-
-    private void setInputDataParams(Fraction[][] restrictions,  Fraction[] mainFunc,
-                                    Simplex.Sign[] signs, boolean findMax){
-        inputData.restrictionsCoeff = restrictions;
-        inputData.mainFuncCoeff = mainFunc;
-        inputData.comparisonSings = signs;
-        inputData.findMax = findMax;
-    }
 }
