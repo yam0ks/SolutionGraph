640971eb489ba399a607dbd1c84d7b84cae280be
halisha <vadimkaz.alx@gmail.com>
2022-04-06 21:16:02 +0300

    Написал методы под поведение View

--- app/src/main/java/com/viewmodel/MainViewModel.java
+++ app/src/main/java/com/viewmodel/MainViewModel.java
@@ -5,27 +5,25 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import android.os.Bundle;
-import com.model.MainFunc;
+
+import com.model.Fraction;
 import com.model.Restriction;
 import java.util.List;
 import com.model.GraphObjective;
 import com.model.GraphRestriction;
 import com.model.Objective;
-import com.model.Restriction;
 import com.model.constants;
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainViewModel extends ViewModel {
 
-    private MutableLiveData<List<Restriction>> restrictsMutable;
-    public LiveData<List<Restriction>> restricts = restrictsMutable;
+    private MutableLiveData<Restriction[]> restrictsMutable;
+    public LiveData<Restriction[]> restricts = restrictsMutable;
 
 
-    private MutableLiveData<MainFunc> mainFunctionMutable;
-    public LiveData<MainFunc> mainFunction = mainFunctionMutable;
+    private MutableLiveData<Objective> mainFunctionMutable;
+    public LiveData<Objective> mainFunction = mainFunctionMutable;
 
-    }
 
     public void GraphSolution(Bundle bundle){
 
@@ -65,4 +63,68 @@ public class MainViewModel extends ViewModel {
 
         return new GraphObjective(x_value, y_value, goal);
     }
+
+    public void changeRestrictCoeffs(int restrictIndex, int coeffIndex, double newValue) {
+        Restriction[] restrictsNormal = restrictsMutable.getValue();
+        Fraction[] fracsNormal = restrictsNormal[restrictIndex].getFractionCoeffs();
+        fracsNormal[coeffIndex] = new Fraction(newValue);
+        restrictsNormal[restrictIndex].setFractionCoeffs(fracsNormal);
+        restrictsMutable.setValue(restrictsNormal);
+    }
+
+    public void changeRestrictFreeCoeff(int restrictIndex, double newValue) {
+        Restriction[] restrictionsNormal = restrictsMutable.getValue();
+        restrictionsNormal[restrictIndex].freeCoeff = new Fraction(newValue);
+        restrictsMutable.setValue(restrictionsNormal);
+    }
+
+    public void changeRestrictRes(int restrictIndex, double newValue) {
+        Restriction[] restrictionsNormal = restrictsMutable.getValue();
+        restrictionsNormal[restrictIndex].result = new Fraction(newValue);
+        restrictsMutable.setValue(restrictionsNormal);
+    }
+
+    public void changeRestrictSign(int restrictIndex, constants.Sign newSign) {
+        Restriction[] restrictionsNormal = restrictsMutable.getValue();
+        restrictionsNormal[restrictIndex].sign = newSign;
+        restrictsMutable.setValue(restrictionsNormal);
+    }
+
+    public void changeMainFuncCoeff(int coeffIndex, double newValue) {
+        Objective mainFunc = mainFunctionMutable.getValue();
+        Fraction[] fracsNormal = mainFunc.getFractionCoeffs();
+        fracsNormal[coeffIndex] = new Fraction(newValue);
+        mainFunc.setFractionCoeffs(fracsNormal);
+        mainFunctionMutable.setValue(mainFunc);
+    }
+
+    public void changeMainFuncGoalType(constants.GoalType goal) {
+        Objective mainFunc = mainFunctionMutable.getValue();
+        mainFunc.goal_type = goal;
+        mainFunctionMutable.setValue(mainFunc);
+    }
+
+    public void createRestrictionData(int restrictionCount, int variableCount) {
+        Restriction[] restrictionList = new Restriction[restrictionCount];
+        double[] sampleCoeffs = new double[variableCount];
+        for (int i = 0; i < variableCount; i++){
+            sampleCoeffs[i] = 1;
+        }
+        for (int i = 0; i < restrictionCount; i++) {
+            restrictionList[restrictionCount] = new Restriction(sampleCoeffs, 0, constants.Sign.EQUALS, 0);
+        }
+        restrictsMutable.setValue(restrictionList);
+    }
+
+    public void createMainFuncData(int variableCount) {
+        double[] sampleCoeffs = new double[variableCount];
+        for (int i = 0; i < variableCount; i++) {
+            sampleCoeffs[i] = 1;
+        }
+        Objective mainFunc = new Objective(sampleCoeffs, constants.GoalType.MAXIMIZE);
+        mainFunctionMutable.setValue(mainFunc);
+    }
+
+
+
 }
