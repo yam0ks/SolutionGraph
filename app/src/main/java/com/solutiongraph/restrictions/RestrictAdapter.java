9fb0c10f2c265e1a5f98505f75fbad19623c6425
ToxicCreator <aleks7000bol@gmail.com>
2022-04-10 15:53:40 +0300

    Установил числовой ввод для коэффициентов

--- app/src/main/java/com/solutiongraph/restrictions/RestrictAdapter.java
+++ app/src/main/java/com/solutiongraph/restrictions/RestrictAdapter.java
@@ -10,17 +10,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.model.simplexdata.Restriction;
 import com.solutiongraph.R;
-import com.solutiongraph.restrictions.RestrictViewHolder;
 import com.utils.Constants;
 
 public class RestrictAdapter extends RecyclerView.Adapter<RestrictViewHolder> {
-    private final Context context;
     private final LayoutInflater layoutInflater;
     private final Restriction[] restrictions;
     private final int numbersCount;
 
     public RestrictAdapter(Context context, Restriction[] data, int numbersCount) {
-        this.context = context;
         this.restrictions = data;
         this.layoutInflater = LayoutInflater.from(context);
         this.numbersCount = numbersCount;
@@ -37,25 +34,26 @@ public class RestrictAdapter extends RecyclerView.Adapter<RestrictViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull RestrictViewHolder holder, int position) {
         Restriction restrict = this.restrictions[position];
+        holder.setHeaderText(restrict);
+
         double[] coeffs = restrict.getDoubleCoeffs();
         double freeCoeff = restrict.getFreeCoeffAsDouble();
-        double result = restrict.getResultCoeffAsDouble();
+        double result = restrict.getResultAsDouble();
         Constants.Sign sign = restrict.getSign();
 
-        holder.result.setText(String.valueOf(result));
-        holder.freeCoeffView.setText(String.valueOf(freeCoeff));
-        switch (sign.name()){
-            case ("LESS"):
-                holder.signView.check(R.id.radio_less);
+        holder.setResult(result);
+        holder.setFreeCoeff(freeCoeff);
+        switch (sign){
+            case LESS:
+                holder.signCheck(R.id.radio_less);
                 break;
-            case ("EQUAL"):
-                holder.signView.check(R.id.radio_equal);
+            case EQUALS:
+                holder.signCheck(R.id.radio_equal);
                 break;
-            case ("MORE"):
-                holder.signView.check(R.id.radio_more);
+            case MORE:
+                holder.signCheck(R.id.radio_more);
                 break;
         }
-        holder.setHeader(coeffs, freeCoeff, sign, result);
     }
 
     @Override
